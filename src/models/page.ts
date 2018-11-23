25c40ddf53bc50cd917073fcd7b5bb45f3027a6c
Daniel Pereira <daniel.prr.azevedo>
2018-11-23 19:11:26 -0200

    base page

---
+++ src/models/page.ts
@@ -0,0 +1,12 @@
+export interface Page<T> {
+    current_page: number;
+    data: T[];
+    from: number;
+    last_page: number;
+    next_page_url: string;
+    path: string;
+    per_page: number;
+    prev_page_url: string;
+    to: number;
+    total: number;
+}
